f9ac23a8412a1b2a29e695ac3f50bc1f0f624a51
Irsyad Firmansyah <irsyadjelek35@gmail.com>
2025-09-09 01:40:32 +0700

    yes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   base: '/F1ru14.github.io/',
 })
